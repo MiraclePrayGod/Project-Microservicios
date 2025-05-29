9d344b173a27067d2b767f3de709b493c98da4a1
MiraclePrayGod <reginaldobuendia@gmail.com>
2025-05-29 01:58:31 -0500

    gooo

--- dp-licencias/src/main/java/contacloud/dplicencias/DpLicenciasApplication.java
+++ dp-licencias/src/main/java/contacloud/dplicencias/DpLicenciasApplication.java
@@ -3,7 +3,6 @@ package contacloud.dplicencias;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @EnableFeignClients
 @SpringBootApplication
