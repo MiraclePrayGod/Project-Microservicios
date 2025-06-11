45412943ed5498a2a19911abcdef2f4c9f2183e0
MiraclePrayGod <reginaldobuendia@gmail.com>
2025-06-11 04:51:36 +0200

    goo

---
+++ dp-inventario/src/main/java/com/contacloud/dpinventario/DpInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.contacloud.dpinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpInventarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpInventarioApplication.class, args);
+    }
+
+}
---
+++ dp-inventario/src/test/java/com/contacloud/dpinventario/DpInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contacloud.dpinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpInventarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
